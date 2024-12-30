fix: little masking improvement

app.py
@@ -4,6 +4,7 @@ from rembg import remove
 from PIL import Image
 from io import BytesIO
 from dotenv import load_dotenv
+import numpy as np
 
 load_dotenv()
 
@@ -15,6 +16,12 @@ WEBHOOK_URL = os.getenv('DISCORD_WEBHOOK_URL')
 # extensi file yang diperbolehkan
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
+# fungsi untuk improve masking gambar
+def improve_mask(mask, erosion_factor=5):
+    from scipy import ndimage
+    mask = ndimage.binary_erosion(mask, iterations=erosion_factor)
+    mask = ndimage.binary_dilation(mask, iterations=erosion_factor)
+    return mask
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
@@ -42,7 +49,20 @@ def upload_file():
         if file:
             try:
                 input_image = Image.open(file.stream)
-                output_image = remove(input_image, post_process_mask=True)
+                # remove background dengan parameter tambahan
+                output = remove(
+                    input_image,
+                    alpha_matting=True,
+                    alpha_matting_foreground_threshold=240,
+                    alpha_matting_background_threshold=10,
+                    post_process_mask=improve_mask
+                )
+                # ngubah numpy jadi array
+                output_array = np.array(output)
+                # menaikan nilai alpha menjadi lebih tinggi
+                output_array[:, :, 3] = np.clip(output_array[:, :, 3] * 1.2, 0, 255)
+                # ubah kemabli menjadi gambar
+                output_image = Image.fromarray(output_array)
                 img_io = BytesIO()
                 output_image.save(img_io, 'PNG')
                 img_io.seek(0)
@@ -50,6 +70,7 @@ def upload_file():
                 return send_file(img_io, mimetype='image/png', as_attachment=True, download_name=downName)
             except Exception as e:
                 return jsonify({'error': f'Error processing image: {str(e)}'}), 500
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=5100)