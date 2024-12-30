5b1bfaf42f5571bdd8ab76e6efe15cac09a44f7d
Schutz3 <02.farhan.dwi@gmail.com>
2024-12-30 09:26:35 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Zee Remove BG
+A simple flask app to remove the background of an image with [Rembg](https://github.com/danielgatis/rembg)
+
+## Deployed site
+
+[ZeeRMBG](https://bg.scz.my.id/)
+
+## Features
+
+- Easy-to-use web interface
+- Drag and drop or click to upload images
+- Supports PNG, JPG, and JPEG file formats
+- Maximum file size of 10MB
+- Preview of original and processed images
+- One-click download of the processed image
+
+## Tech Stack
+
+- Python
+- Flask
+- Rembg
+- Pillow
+- HTML/CSS/JavaScript
+- Tailwind CSS
+- DaisyUI
+
+## Getting Started
+
+1. Clone the repository
+2. Install dependencies: `pip install -r requirements.txt`
+3. Run the app: `python app.py`
+4. Open your browser and navigate to `http://localhost:5100`
+
+## Usage
+
+1. Upload an image by dragging and dropping or clicking the "Select Image" button
+2. Wait for the background removal process to complete
+3. Preview the original and processed images
+4. Click the "Download" button to save the processed image
+
+## Author
+
+[Zee](https://scz.my.id/)
---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, send_file, jsonify
+from rembg import remove
+from PIL import Image
+from io import BytesIO
+
+app = Flask(__name__)
+
+# max file 
+MAX_FILE_SIZE = 10 * 1024 * 1024  # 10 mb dalam bytes
+
+# extensi file yang diperbolehkan
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return jsonify({'error': 'No file uploaded'}), 400
+        
+        file = request.files['file']
+        
+        if file.filename == '':
+            return jsonify({'error': 'No file selected'}), 400
+        
+        if not allowed_file(file.filename):
+            return jsonify({'error': 'Invalid file type. Allowed types are png, jpg, jpeg'}), 400
+        
+        if request.content_length > MAX_FILE_SIZE:
+            return jsonify({'error': 'File size exceeds the maximum limit of 10 MB'}), 400
+        
+        if file:
+            try:
+                input_image = Image.open(file.stream)
+                output_image = remove(input_image, post_process_mask=True)
+                img_io = BytesIO()
+                output_image.save(img_io, 'PNG')
+                img_io.seek(0)
+                downName = file.filename+'_Zee-rmbg.png'
+                return send_file(img_io, mimetype='image/png', as_attachment=True, download_name=downName)
+            except Exception as e:
+                return jsonify({'error': f'Error processing image: {str(e)}'}), 500
+    
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, port=5100)
